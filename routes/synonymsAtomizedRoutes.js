8a205d5c49be75fd653b829bd586ccbb9d2bcdaf
cosmicgarden <cosmicgarden1@gmail.com>
2016-02-25 16:26:44 -0500

    save SynonymsAtomizd by version

--- routes/synonymsAtomizedRoutes.js
+++ routes/synonymsAtomizedRoutes.js
@@ -9,7 +9,7 @@ var cors = require;
 router.post('/post', function(req, res) {
   var synonyms_atomized_version  = req.body; 
   synonyms_atomized_version._id = mongoose.Types.ObjectId();
-  console.log(req.body); 
+  //console.log(req.body); 
 
   //Verify if the Json have  a RecordId(ID de la Ficha). If the Json don't have a id for record this will be created
   if(typeof  synonyms_atomized_version.id_record==="undefined" || synonyms_atomized_version.id_record==""){
@@ -17,7 +17,11 @@ router.post('/post', function(req, res) {
   }
 
   var count_record=0;
-  add_objects.RecordVersion.count({ _id : synonyms_atomized_version.id_record }, function (err, count){ 
+
+  var test=0;
+
+  test=add_objects.RecordVersion.count({ _id : synonyms_atomized_version.id_record }, function (err, count){ 
+    console.log("EL conteo: "+count);
     count_record=count;
     if(count==0){
         console.log("No exist record with id: "+synonyms_atomized_version._id);
@@ -26,25 +30,28 @@ router.post('/post', function(req, res) {
 
   console.log("Numero de fichas encontradas con id: "+count_record);
 
-  var count_version= 0;
+  console.log("TEST Numero de fichas encontradas con id: "+test);
+
+  
   if(count_record==1){
 
     add_objects.RecordVersion.findById(synonyms_atomized_version._id, function(err, doc){
       if (err)
-        res.send(err);
+        res.send(err); //to do: throw error or message
       if(!doc){
         res.send("Document not found");
       }
+      console.log("La ficha: "+RecordVersion);
       count_version=doc.synonymsAtomizedVersion.length;
     });
   };
 
-  console.log("Numero de versiones: "+ count_version);
+  //console.log("Numero de versiones: "+ count_version);
 
   synonyms_atomized_version._id=mongoose.Types.ObjectId();
-  synonyms_atomized_version.version=count_version+1;
-  console.log(typeof SynonymsAtomizedVersion);
-  console.log(Object.keys(SynonymsAtomizedVersion));
+  //synonyms_atomized_version.version=count_version+1;
+  //console.log(typeof SynonymsAtomizedVersion);
+  //console.log(Object.keys(SynonymsAtomizedVersion));
   synonyms_atomized_version = new SynonymsAtomizedVersion(synonyms_atomized_version);
 
   var id_v = synonyms_atomized_version._id;
@@ -52,21 +59,36 @@ router.post('/post', function(req, res) {
   var id_rc = synonyms_atomized_version.id_record; 
 
   synonyms_atomized_version.save(function(err) {
-            if (err)
-                res.send(err);
+    
+    if (err){
+      res.send(err);
+    } 
+    var count_record= 0; 
+    add_objects.RecordVersion.count({ _id : synonyms_atomized_version.id_record }, function (err, count){ 
+      console.log("EL conteo: "+count);
+      count_record=count;
+      if(count==0){
+        console.log("No exist record with id: "+synonyms_atomized_version._id);
+      }
+    });  
+
+    console.log("El nuevo conteo: "+count_record);
+             /* 
             add_objects.RecordVersion.findByIdAndUpdate( id_rc, { $push: { "synonymsAtomizedVersion": id_v } },{ safe: true, upsert: true },function(err, model) {
                      if (err)
                        res.send(err);
                      res.json({ message: 'Save SynonymsAtomizedVersion!' });
                      console.log("Save SynonymsAtomizedVersion!");
             });
-            /*  
-            if(count_record==1){
+            */
+              
+            if(true){
               add_objects.RecordVersion.findByIdAndUpdate( synonyms_atomized_version.id_record, { $push: { "synonymsAtomizedVersion": synonyms_atomized_version._id } },{safe: true, upsert: true},function(err, model) {
                   	 if (err)
                     	 res.send(err);
                   	 res.json({ message: 'Save SynonymsAtomizedVersion!' });
                   	 console.log("Save SynonymsAtomizedVersion!");
+                     console.log("EL Modelo: "+model);
               }); 
             }else{
               var ob_ids= new Array();
@@ -78,7 +100,7 @@ router.post('/post', function(req, res) {
                 console.log("Created a new Record and Save SynonymsAtomizedVersion!!");
               });
             }
-            */
+            
         });
 });
 
