7619dd9796ccecf43c2fb66d5f573fc05d635d92
cosmicgarden <cosmicgarden1@gmail.com>
2016-04-01 16:23:12 -0500

    changes for migration

--- app/migration/passNewDBTest.js
+++ app/migration/passNewDBTest.js
@@ -13,55 +13,39 @@ var Schema = mongoose.Schema;
 function moveDocuments(){
 
   var conTemp = mongoose.createConnection('mongodb://localhost:27017/editorDb', function(err) {
-    	if(err) {
-        	console.log('connection error', err);
-    	} else {
-        	console.log('connection successful Temp Database');
-    	}
-	});
+      if(err) {
+          console.log('connection error', err);
+      } else {
+          console.log('connection successful Temp Database');
+      }
+  });
 
-	var conNew = mongoose.createConnection('mongodb://localhost:27017/catalogoDB', function(err) {
-    	if(err) {
-        	console.log('connection error', err);
-    	} else {
-        	console.log('connection successful New Database');
-    	}
-	});
+  var recordSchema = new Schema({name:String}, { strict: false, versionKey: false });
 
-	var recordSchema = new Schema({name:String}, { strict: false, versionKey: false });
+  var RecordModel = conTemp.model('Record', recordSchema);
 
-	var RecordModel = conTemp.model('Record', recordSchema);
-
-	var newRecordSchema = add_objects.RecordVersion.schema;
-  	var texSchema = TaxonRecordNameVersion.schema;
-  	var moreInformationSchema = MoreInformationVersion.schema;
-
-  	var newRecordModel = conNew.model('RecordVersion', newRecordSchema );
-  	var texModel = conNew.model('TaxonRecordNameVersion', texSchema );
-  	var moreInformationModel = conNew.model('MoreInformationVersion', moreInformationSchema );
-
-  	RecordModel.find({}, function (err, data){
-  		if(err){
-  			console.log(err);
-  		}else{
-  			for(var i=0;i<data.length;i++){
+  RecordModel.find({}, function (err, data){
+      if(err){
+        console.log(err);
+      }else{
+        for(var i=0;i<data.length;i++){
           /*
           console.log(data[i]._id);
           console.log(data[i]._doc.taxonRecordName);
           console.log(Object.keys(data[i]));
           */
           
-  				var id_record = data[i]._id;
+          var id_record = data[i]._id;
           //connection, RecordModel, element, id_record
           taxon_record_name.postRecord(conNew, newRecordModel, data[i]._doc.taxonRecordName, id_record);
           more_information.postVersion(conNew, newRecordModel, data[i]._doc.moreInformation, id_record);
           associated_party.postVersion(conNew, newRecordModel, data[i]._doc.associatedParty, id_record);
-  			}
-  		}
-  	});
+        }
+      }
+    });
 
-  	
-  	return;
+  conTemp=mongoose.disconnect();
+  return;
 
 }
 
