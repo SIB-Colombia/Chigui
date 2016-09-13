d988cb67b1ae88a6444e7887f5bc77d5506e50f5
cosmicgarden <cosmicgarden1@gmail.com>
2016-09-13 16:25:25 -0500

    modification collection controller

--- api/controllers/collection.js
+++ api/controllers/collection.js
@@ -8,56 +8,84 @@ function postCollection(req, res) {
 	var object_id = mongoose.Types.ObjectId();
 	collection._id = mongoose.Types.ObjectId();
 	collection.created=Date();
-	console.log(collection);
-	console.log("*** "+collection.records.length);
+	//console.log(collection);
+	//console.log("*** "+collection.records.length);
 	
+	var isValidArr = true; 
+	var reg_id = new RegExp("^[0-9a-fA-F]{24}$");
 	for(var i =0; i < collection.records.length; i++){
-		console.log(object_id.isValid(records[i]));
-		//collection.records[i] = mongoose.Types.ObjectId(collection.records[i]);
+		console.log(reg_id.test(collection.records[i]));
+		if (!reg_id.test(collection.records[i])){
+			isValidArr = false;
+			break;
+		}else{
+			collection.records[i] = mongoose.Types.ObjectId(collection.records[i]); 
+		}
 	}
+
 	collection = new Collection(collection);
 
 	console.log(collection);
 
 	console.log(collection.records.length);
 
-	if(collection.records.length > 0){
-		console.log(collection.records.length);
-		async.waterfall([
+	if(isValidArr){
+		if(collection.records.length > 0){
+			console.log(collection.records.length);
+			async.waterfall([
 				function(callback){
 					async.eachSeries(collection.records, function(id_record, callback){
+						console.log(id_record);
 						add_objects.RecordVersion.count({ _id : id_record }, function (err, count){
             				if(err){
               					callback(new Error("The Record (Ficha) with id: "+id_record+" doesn't exist.:" + err.message));
-           			 		}else if(count > 1){
+           			 		}else if(count === 0){
            			 			callback(new Error("The Record (Ficha) with id: "+id_record+" doesn't exist"));
+           			 		}else{
+           			 			callback();
            			 		}
           				});
 					},function(err){
 						if(err){
-							console.error("Error finding a Record: "+err.message);
+							//console.error("Error finding a Record: "+err.message);
+							callback(new Error("Error finding a Record: "+err.message));
 						}else{
 							console.log("All Records are in the dataBase");
 							callback();
 						}
 					});
+				},
+				function(callback){
+					collection.save(function(err){
+                  		if(err){
+                      		callback(new Error("failed saving the collection:" + err.message));
+                  		}else{
+                  			console.log("!!!!");
+                      		callback(null);
+                  		}
+                	});
 				}
-			],
-			function(err, result) {
-          		if (err) {
-            		console.log("Error: "+err);
-            		//res.status(406);
-            		res.status(400);
-            		res.json({ ErrorResponse: {message: ""+err }});
-          		}else{
-            		res.json({ message: 'Save Collection', collection_id: collection._id  });
-         		}
-			}
-		);
-	}else{
+				],
+				function(err, result) {
+          			if (err) {
+            			console.log(err.message);
+            			//res.status(406);
+            			res.status(400);
+            			res.json({ ErrorResponse: {message: ""+err }});
+          			}else{
+            			res.json({ message: 'Save Collection', collection_id: collection._id  });
+         			}
+				}
+			);
+		}else{
 		//res.status(406);
     	res.status(400);
     	res.json({message: "The array of the id's of Records is empty"});
+		}
+	}else{
+		//res.status(406);
+    	res.status(400);
+    	res.json({message: "The array of the id's of Records have an incorrect id"});
 	}
 }
 
--- api/models/collection.js
+++ api/models/collection.js
@@ -3,7 +3,7 @@ var extend = require('mongoose-schema-extend');
 var Schema = mongoose.Schema;
 var ad_objects = require('./additionalModels.js');
 
-var Collection = new Schema ({
+var RecordCollection = new Schema ({
 	id_user : String,
 	records : [{ type: Schema.Types.ObjectId, ref: 'RecordVersion' }],
 	created : { type: Date, default: Date.now },
@@ -11,6 +11,6 @@ var Collection = new Schema ({
 	description : String,
 	tags : [String],
 	groups : [{ type: Schema.Types.ObjectId, ref: 'Groups' }]
-},{ collection: 'Collections' });
+},{ collection: 'RecordCollections' });
 
-module.exports = mongoose.model('Collection', Collection );
+module.exports = mongoose.model('RecordCollection', RecordCollection );
