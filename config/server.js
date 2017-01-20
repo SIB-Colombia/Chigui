e45fc38e5ce371ea9e4ea40678d04757d222cd6d
cosmicgarden <cosmicgarden1@gmail.com>
2017-01-19 21:37:00 -0500

    changed values for connection

--- config/server.js
+++ config/server.js
@@ -1,8 +1,18 @@
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/catalogoDb', function(err) {
+
+var options = {
+  user: 'oscar',
+  pass: '1323Oscar2572'
+}
+
+mongoose.connect('mongodb://localhost:27017/catalogoDb', options, function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
         console.log('connection successful');
     }
 });
+
+
+//mongoose.connect(uri, options);
+
