c9fb31ae98c216ec63a72a31e4ea3161639bd802
cosmicgarden <cosmicgarden1@gmail.com>
2017-03-02 10:28:48 -0500

    added docker file

--- ISSUE_TEMPLATE.md
+++
@@ -1,7 +0,0 @@
-1. Title the issue in a direct, informative and short way. 
-2. Write down the browser used, e.g. "browser:firefox", "browser:safari".
-3. @members only if needed.
-4. In as much detail as possible describe the problem, question, help or task to be addressed.
-5. Attach a picture only if needed.
-6. Always assing the proper labels following the label guide in the CBC wiki, please don't be lazy.
-7. Only if you're aware of the milestone please assign it.
--- config/server.js
+++ config/server.js
@@ -1,15 +1,17 @@
 var mongoose   = require('mongoose');
 
+/*
 var options = {
   user: 'oscar',
   pass: '1323Oscar2572'
 }
+*/
 
-mongoose.connect('mongodb://localhost:27017/catalogoDb', options, function(err) {
+mongoose.connect('mongodb://localhost:27017/catalogoDb', function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
-        console.log('connection successful');
+        console.log('connection successful to the database');
     }
 });
 
