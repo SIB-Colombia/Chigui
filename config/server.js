9e75831a79f56641e12e554c551696fd8b3d9023
Oscar Javier Duque <cosmicgarden1@gmail.com>
2017-09-26 17:48:22 -0500

    change url
    change url

--- config/server.js
+++ config/server.js
@@ -1,11 +1,7 @@
 var mongoose   = require('mongoose');
 
-var options = {
-  user: 'testcatalogo',
-  pass: '1323catalogotest'
-}
 
-mongoose.connect('mongodb://54.82.249.77:27017/catalogoDbTest', options, function(err) {
+mongoose.connect('mongodb://localhost:27017/catalogoDbTest', options, function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
